d17583320204ac5e55914e3d32a61f6bd0ae0df0
trabdlkarim <trabdlkarim@programmer.net>
2020-09-23 02:45:36 +0300

    updated

---
+++ browser/vocebrowser/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 18:18:09 2020
+
+@author: trabdlkarim
+"""
+import os
+
+QRC_DIR = os.path.dirname(__file__)
--- browser/vocebrowser/browser.py
+++ browser/vocebrowser/browser.py
@@ -7,10 +7,11 @@ Created on Mon Sep 21 18:09:34 2020
 """
 
 import sys
+from PyQt5 import Qt
 from browserui import BrowserUi
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWebEngineWidgets import QWebEngineView , QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView , QWebEnginePage, QWebEngineSettings
 
 
 class VoceBrowser(QtWidgets.QMainWindow):
@@ -47,15 +48,20 @@ class VoceBrowser(QtWidgets.QMainWindow):
             url = QUrl("about:blank")
         webView = QWebEngineView()
         webView.setUrl(url)
+        webView.setFocus()
         index = self.ui.tabWidget.addTab(webView, title)
         self.ui.tabWidget.setCurrentIndex(index)
         webView.urlChanged.connect(lambda qurl, view = webView: self.update_address_bar(qurl, view))
         webView.loadFinished.connect(lambda _, i = index, view = webView:self.update_tab_title(i, view))
+        webView.iconChanged.connect(lambda _,i=index,view=webView:self.update_favIcon(i,view))
+
+    def update_favIcon(self,tabIndex,webView):
+        self.ui.tabWidget.setTabIcon(tabIndex, webView.icon())
 
 
-    def update_tab_title(self, tabindex, view):
-        title = view.page().title()
-        self.ui.tabWidget.setTabText(tabindex, title)
+    def update_tab_title(self, tabIndex, webView):
+        title = webView.page().title()
+        self.ui.tabWidget.setTabText(tabIndex, title)
         self.setWindowTitle("%s - %s" % (title,self.appName))
 
     def onchange_current_tab(self, i):
@@ -89,23 +95,41 @@ class VoceBrowser(QtWidgets.QMainWindow):
         self.ui.tabWidget.currentWidget().setUrl(self.home_url)
 
     def goto_current_url(self):
-        current_url = QUrl(self.ui.urlbar.text())
-        if current_url.scheme == "":
-            current_url.setScheme('http')
+        current_url = QUrl.fromUserInput(self.ui.urlbar.text())
+        if current_url.isValid():
+            if not '.' in self.ui.urlbar.text():
+                current_url = self.create_search_query("https://duckduckgo.com",self.ui.urlbar.text())
+        else:
+            current_url = self.create_search_query("https://duckduckgo.com",self.ui.urlbar.text())
+
         self.ui.tabWidget.currentWidget().setUrl(current_url)
+
+    def create_search_query(self,engine,keywords):
+        query = engine + "?q="
+        words = keywords.split()
+        count = len(words)
+        for w in words:
+            if count>1:
+                query += w + '+'
+            else:
+                query += w
+            count -= 1
+        return QUrl(query)
+
 
 
 
 def main(argv):
-   app = QtWidgets.QApplication(argv)
-   browser = VoceBrowser()
-   app.setApplicationName(browser.appName)
 
-   browser.show()
-   sys.exit(app.exec_())
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
 
+    app = QtWidgets.QApplication(argv)
+    app.setOrganizationName("Voce Browser Fundation")
+    QWebEngineSettings.defaultSettings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+    browser = VoceBrowser()
+    app.setApplicationName(browser.appName)
 
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    browser.show()
+    sys.exit(app.exec_())
 

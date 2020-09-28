42729a4a31a05de4fdf9ac475ad800c13ebf4930
trabdlkarim <trabdlkarim@programmer.net>
2020-09-28 23:49:47 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1a2.dev',  # Required
+    version='0.0.1a3.dev',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:

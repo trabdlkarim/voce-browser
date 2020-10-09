3d80aafb209fbc27f16b59cccfc4e924216f4b8b
trabdlkarim <trabdlkarim@programmer.net>
2020-10-09 11:12:25 +0300

    upate app

--- lib/vocebrowser/app.py
+++ lib/vocebrowser/app.py
@@ -6,37 +6,43 @@ Created on Mon Sep 21 17:57:00 2020
 @author: trabdlkarim
 """
 
+import sys
+
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
-import sys
 from PyQt5.QtWebEngineWidgets import  QWebEngineSettings
+from PyQt5.QtWebEngineWidgets import QWebEngineProfile
+
 from vocebrowser.browser import VoceBrowser
-
-
-class Voce(object):
-      def __init__(self):
-          pass
-
-      def run(self,argv):
-          pass
+from vocebrowser.schemes import VoceUrlSchemeHandler, ResourceUrlSchemeHandler
 
 def main(argv=sys.argv):
 
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
 
+    VoceUrlSchemeHandler.registerVoceUrlScheme()
+    ResourceUrlSchemeHandler.registerVoceUrlScheme()
+
     app = QtWidgets.QApplication(argv)
 
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
-    QtCore.QCoreApplication.setOrganizationName('Voce Browser Community')
+    QtCore.QCoreApplication.setOrganizationName('Voce Browser Fundation')
     QWebEngineSettings.defaultSettings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
 
     app.setApplicationName(VoceBrowser.appName)
+    browser = VoceBrowser()
 
-    voce = VoceBrowser()
-    voce.launch()
+    voceUrlSchemeHandler = VoceUrlSchemeHandler()
+    resourceUrlSchemeHandler = ResourceUrlSchemeHandler()
 
+    QWebEngineProfile.defaultProfile().installUrlSchemeHandler(VoceUrlSchemeHandler.schemeName, voceUrlSchemeHandler)
+    QWebEngineProfile.defaultProfile().installUrlSchemeHandler(ResourceUrlSchemeHandler.schemeName, resourceUrlSchemeHandler)
+
+
+    browser.launch()
 
     return sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
